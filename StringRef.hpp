0b5e6be8e1a17fbbeb0975821ad93a1f5142666a
GiM <gim913@gmail.com>
2012-10-25 23:47:25 +0200

    initial stuff seems to be working

--- StringRef.hpp
+++ StringRef.hpp
@@ -74,26 +74,66 @@ namespace {
 	{
 		StringPool_(const StringPool_&);             // you definitely neither do want a copy
 		StringPool_& operator=(const StringPool_&);  // nor assignemnt ;p
-		typedef StringRef_<T> StringRef;
+		typedef StringPool_<T> MyStringPool;
+		typedef StringRef_<T> MyStringRef;
 	public:
-		StringPool_() : m_offset(0) {}
+		StringPool_() : m_currentOffset(0) {}
 
 		// return 'id' of added element
-		size_t push_back(const StringRef& stringRef) {
+		size_t push_back(const MyStringRef& stringRef) {
 			const uint8_t* b = reinterpret_cast<const uint8_t*>( stringRef.begin() );
 			const uint8_t* e = reinterpret_cast<const uint8_t*>( stringRef.end() );
 			m_memory.insert(m_memory.end(), b, e);
 
-			m_offset.push_back(m_currentOffset);
+			printf("%2d %ws\n", m_currentOffset, b);
+			m_entries.push_back(StringPoolEntry(m_currentOffset, (e-b)));
 			m_currentOffset += (e-b);
 
-			return m_offset.size()-1;
+			return m_entries.size()-1;
 		}
 
+		struct EntriesIterator;
+		typedef EntriesIterator iterator;
+		iterator begin() {
+			return iterator(*this, m_entries.begin());
+		}
+		iterator end() {
+			return iterator(*this, m_entries.end());
+		}
 	private:
+		struct StringPoolEntry {
+			StringPoolEntry() : offset(0), size(0) {}
+			StringPoolEntry(size_t a_offset, size_t a_size) : offset(a_offset), size(a_size) {}
+			size_t offset;
+			size_t size;
+		};
+		struct EntriesIterator {
+			typedef typename std::vector<StringPoolEntry>::iterator It;
+			EntriesIterator(MyStringPool& a_pool, It a_it) : pool(a_pool), it(a_it) {}
+			bool operator==(const EntriesIterator& rhs) {
+				return ((&pool == &(rhs.pool)) && (it == rhs.it));
+			}
+			bool operator!=(const EntriesIterator& rhs) {
+				return !(*this == rhs);
+			}
+
+			MyStringRef operator*() {
+				printf ("%d\n", (*it).offset);
+				const T* ptr = reinterpret_cast<const T*>( &(pool.m_memory[(*it).offset]) );
+				size_t len = (*it).size / sizeof(T)  - 1;
+				return MyStringRef(ptr, len);
+			}
+
+			EntriesIterator& operator++() {
+				++it;
+				return *this;
+			}
+			MyStringPool& pool;
+			It it;
+		};
 		size_t m_currentOffset;
 		std::vector<uint8_t> m_memory;
-		std::vector<size_t> m_offset;
+		std::vector<StringPoolEntry> m_entries;
 	};
 }
 
